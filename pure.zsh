6f4d61b70c334ae0f8439af914d7579fa6c4c757
Victor Ferraz <victorfsf.dev@gmail.com>
2017-11-08 15:00:46 -0300

    [master] Changing arrow's color from magenta to green

--- pure.zsh
+++ pure.zsh
@@ -466,7 +466,7 @@ prompt_pure_setup() {
 	PROMPT='%(12V.%F{cyan}%12v%f .)'
 
 	# prompt turns red if the previous command didn't exit with 0
-	PROMPT+='%(?.%F{magenta}.%F{red})${PURE_PROMPT_SYMBOL:-❯}%f '
+	PROMPT+='%(?.%F{green}.%F{red})${PURE_PROMPT_SYMBOL:-❯}%f '
 }
 
 prompt_pure_setup "$@"
